a5571f4bfd06cd7882a829e0481a72c2b9b7066c
João Lucas <99347151+pokergames112@users.noreply.github.com>
2022-02-15 08:59:09 -0300

    commit

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ function contar(){
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
-        window.alert('[Erro] Faltam dados!')
-    } else {
-        res.innerHTML = 'Contando: '
-        let i = Number(ini.value)  
+        window.alert('[ERRO] Faltando dados')
+    } else{
+        res.innerHTML = ('Contando: ')
+        let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
         for(let c = i; c <= f; c += p){
-            res.innerHTML += `${c}`
+            res.innerHTML += `${c} `
         }
     }
 }
